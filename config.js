e167f9bfc1cf51601efce6ecb90a424f47796237
shufangyi <102338496@qq.com>
2018-08-31 13:52:40 +0800

    add trending config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   API_BASE: 'https://www.shufangyi.cn/github',
+  TRENDING_BASE: 'https://www.shufangyi.cn/trending',
   GITHUB_KEY: '764e0aec9dad29bfbf5b995736d415f05114bddb'
 }
