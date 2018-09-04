e244680a108c39dfbe97a571d68dc5a5dd61a4a0
shufangyi <102338496@qq.com>
2018-09-04 15:12:03 +0800

    [fix]:readme page icon style

--- components/wemark/wemark.js
+++ components/wemark/wemark.js
@@ -1,55 +1,63 @@
-const parser = require('./parser');
-const getRichTextNodes = require('./richtext').getRichTextNodes;
-
+const parser = require('./parser')
+const getRichTextNodes = require('./richtext').getRichTextNodes
+let windowWidth = 375
 Component({
-    properties: {
-        md: {
-            type: String,
-            value: '',
-            observer(){
-                this.parseMd();
-            }
-        },
-		type: {
-			type: String,
-			value: 'wemark'
-		},
-		link: {
-			type: Boolean,
-			value: false
-		},
-		highlight: {
-			type: Boolean,
-			value: false
-		}
+  properties: {
+    md: {
+      type: String,
+      value: '',
+      observer() {
+        this.parseMd()
+      }
     },
-    data: {
-        parsedData: {},
-		richTextNodes: []
+    type: {
+      type: String,
+      value: 'wemark'
     },
-    methods: {
-        parseMd(){
-			if (this.data.md) {
-				var parsedData = parser.parse(this.data.md, {
-					link: this.data.link,
-					highlight: this.data.highlight
-				});
-				// console.log('parsedData:', parsedData);
-				if(this.data.type === 'wemark'){
-					this.setData({
-						parsedData
-					});
-				}else{
-					// var inTable = false;
-					var richTextNodes = getRichTextNodes(parsedData);
+    link: {
+      type: Boolean,
+      value: false
+    },
+    highlight: {
+      type: Boolean,
+      value: false
+    }
+  },
+  data: {
+    parsedData: {},
+    richTextNodes: [],
+    images: {}
+  },
+  created() {
+    wx.getSystemInfo({
+      success: function(res) {
+        windowWidth = res.windowWidth
+      }
+    })
+  },
+  methods: {
+    parseMd() {
+      if (this.data.md) {
+        var parsedData = parser.parse(this.data.md, {
+          link: this.data.link,
+          highlight: this.data.highlight
+        })
+        // console.log('parsedData:', parsedData);
+        if (this.data.type === 'wemark') {
+          this.setData({
+            parsedData
+          })
+        } else {
+          // var inTable = false;
+          var richTextNodes = getRichTextNodes(parsedData)
 
-					// console.log('richTextNodes:', richTextNodes);
+          // console.log('richTextNodes:', richTextNodes);
 
-					this.setData({
-						richTextNodes
-					});
+          this.setData({
+            richTextNodes
+          })
 
-					/* // 分批更新
+          /* // 分批更新
 					var update = {};
 					var batchLength = 1000;
 					console.log(batchLength);
@@ -63,9 +71,32 @@ Component({
 					}
 					this.setData(update);
 					update = {}; */
-				}
-
-            }
         }
+      }
+    },
+    imageLoad(e) {
+      var $width = e.detail.width, //获取图片真实宽度
+        $height = e.detail.height,
+        ratio = $width / $height //图片的真实宽高比例
+      let images = this.data.images
+      if (e.target.dataset.shrink === 'wemark_inline_image_tagImg') {
+        const height = 20
+        const width = height * ratio
+        images[e.target.dataset.src] = {
+          height: height + 'px',
+          width: width + 'px'
+        }
+      } else {
+        const width = windowWidth - 20
+        const height = $width / ratio
+        images[e.target.dataset.src] = {
+          height: height + 'px',
+          width: width + 'px'
+        }
+      }
+      this.setData({
+        images: images
+      })
     }
-});
+  }
+})
